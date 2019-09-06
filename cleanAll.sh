701ae77d77b88c38d0894e58be176ccefaaeaa28
Achraf K <achrafkarrouche@gmail.com>
2019-09-06 23:10:43 +0100

    Cleaning script

---
+++ cleanAll.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+make clean;
+make clean-cscope;
+make clean-libLTLIBRARIES;
+make clean-recursive;
+make clean-am;
+make clean-generic;
+make clean-libtool;
+clear;
+echo "**************** All Cleaned ********************";
